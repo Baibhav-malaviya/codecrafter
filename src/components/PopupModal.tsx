dcf7f4c0e1e876b694a494bd9f9c8973c2c447df
Baibhav-malaviya <112685621+Baibhav-malaviya@users.noreply.github.com>
2025-05-02 12:06:25 +0000

    shifting the ref from outler box to inner box

--- src/components/PopupModal.tsx
+++ src/components/PopupModal.tsx
@@ -14,8 +14,8 @@ const PopupModal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
 	const ref = useOutsideClick<HTMLDivElement>(onClose);
 
 	return (
-		<div ref={ref} className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 p-4">
-			<div className="bg-white p-4 rounded-md shadow-lg max-w-md w-full max-h-full overflow-y-auto">
+		<div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 p-4">
+			<div ref={ref} className="bg-white p-4 rounded-md shadow-lg max-w-md w-full max-h-full overflow-y-auto">
 				<div className="flex justify-end">
 					<button
 						className="text-primary bg-slate-800/30 mb-2 rounded-full p-2 font-semibold"
